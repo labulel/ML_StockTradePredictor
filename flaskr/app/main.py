8919b9a1de95d9e04b48df6f0f8f092d5840aa46
preeti0103 <64617207+preeti0103@users.noreply.github.com>
2020-11-21 13:12:08 -0500

    updated main.py file

--- flaskr/app/main.py
+++ flaskr/app/main.py
@@ -1,40 +1,69 @@
-from flask import Flask, render_template, jsonify
+# import os
+# import numpy as np
+# import tensorflow as tf
 
-# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
-import pymongo
+
+
+# pip install pyspark
+# from pyspark.sql import SparkSession
+# spark = SparkSession \
+#     .builder \
+#     .getOrCreate()
+
+    
+# import pyspark
+
+# Import dependencies
+from flask import Flask, render_template, jsonify, request
+
+from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF, StringIndexer
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.linalg import Vector
+from pyspark.ml import Pipeline
+
+from pyspark import SparkContext
+sc = SparkContext("local")
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
-# # connect to the local DB
-# client1 = pymongo.MongoClient("mongodb://preetiv:lY36FjqL2g6eux1c@cluster0-shard-00-00.dj91d.mongodb.net:27017,cluster0-shard-00-01.dj91d.mongodb.net:27017,cluster0-shard-00-02.dj91d.mongodb.net:27017/global_climate?ssl=true&replicaSet=atlas-6s5p4q-shard-0&authSource=admin&retryWrites=true&w=majority")
-# client2 = pymongo.MongoClient("mongodb://preetiv:lY36FjqL2g6eux1c@cluster0-shard-00-00.dj91d.mongodb.net:27017,cluster0-shard-00-01.dj91d.mongodb.net:27017,cluster0-shard-00-02.dj91d.mongodb.net:27017/energy_consumptions?ssl=true&replicaSet=atlas-6s5p4q-shard-0&authSource=admin&retryWrites=true&w=majority")
-# db1 = client1.global_climate
-# db2 = client2.energy_consumptions
 
-# # Set route
-# @app.route('/')
-# def index():
-#     # Return the template with the teams list passed in
-#     return render_template('index.html')#, teams=teams)
+# Create all the features to the data set
+pos_neg_to_num = StringIndexer(inputCol='action',outputCol='label')
+tokenizer = Tokenizer(inputCol="text", outputCol="token_text")
+stopremove = StopWordsRemover(inputCol='token_text',outputCol='stop_tokens')
+hashingTF = HashingTF(inputCol="stop_tokens", outputCol='hash_token')
+idf = IDF(inputCol='hash_token', outputCol='idf_token')
 
-# # Set the route for climate data
-# @app.route('/climate')
-# def climate():
-#     #Pulling the data from the database
-#     climate_data = list(db1.climate_data.find({}, {'_id': False}))
-    
-#     #jsonifying it and returning it to the frontend
-#     return jsonify({'features': climate_data})
+# Create feature vectors
+clean_up = VectorAssembler(inputCols=['idf_token', 'length'], outputCol='features')
 
-# # Set the route for energy data
-# @app.route('/energy')
-# def energy():
-#     #Pulling the data from the database
-#     energy_data = list(db2.energy.find({}, {'_id': False}))
-    
-#     #jsonifying it and returning it to the frontend
-#     return jsonify(energy_data)
+# Create a and run a data processing Pipeline
+data_prep_pipeline = Pipeline(stages=[pos_neg_to_num, tokenizer, stopremove, hashingTF, idf, clean_up])
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+
+# Set route
+@app.route('/', methods=['post', 'get'])
+def index():
+    # Return the template with the teams list passed in
+    headline=''
+    if request.method=='POST':
+        headline = request.form.get('headline')
+
+        #Use headline to make a pysparkdf
+
+        # Fit and transform the pipeline
+        cleaner = data_prep_pipeline.fit(df)
+        cleaned = cleaner.transform(df)
+
+        # model.load('Trade_Predictor_model')
+        # prediction = model.predict(cleaned)
+
+        #transform 0, 1, 2 to buy/hold/sell
+
+    return render_template('index.html', action = prediction)#, teams=teams)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
