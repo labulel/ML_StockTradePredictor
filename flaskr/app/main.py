2a0cf2fe095196bb5c1d95408ab554d722670c8d
preeti0103 <64617207+preeti0103@users.noreply.github.com>
2020-11-23 21:10:45 -0500

    Added comments on main.py file

--- flaskr/app/main.py
+++ flaskr/app/main.py
@@ -1,16 +1,6 @@
-# import os
-# import numpy as np
-# import tensorflow as tf
-
+# Import the dependencies
 import pyspark
-
-# pip install pyspark
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.getOrCreate()
-
-# Import dependencies
 from flask import Flask, render_template, jsonify, request
-
 from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF, StringIndexer
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.linalg import Vector
@@ -18,15 +8,13 @@ from pyspark.ml import Pipeline
 from pyspark.sql.functions import length
 from pyspark import SparkContext
 from pyspark.ml.classification import NaiveBayes, NaiveBayesModel
-# sc = SparkContext("local")
+
+# pip install pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
-
-
-
-
-
 
 # Set route
 @app.route('/', methods=['post', 'get'])
@@ -44,20 +32,16 @@ def index():
         df.show()
 
         # Create all the features to the data set
-        # pos_neg_to_num = StringIndexer(inputCol='action',outputCol='label')
-
         tokenizer = Tokenizer(inputCol="text", outputCol="token_text")
         stopremove = StopWordsRemover(inputCol='token_text',outputCol='stop_tokens')
         hashingTF = HashingTF(inputCol="stop_tokens", outputCol='hash_token')
         idf = IDF(inputCol='hash_token', outputCol='idf_token')
-
 
         # Create feature vectors
         clean_up = VectorAssembler(inputCols=['idf_token', 'length'], outputCol='features')
 
         # Create a and run a data processing Pipeline
         data_prep_pipeline = Pipeline(stages=[tokenizer, stopremove, hashingTF, idf, clean_up])
-
         
         # Fit and transform the pipeline
         cleaner = data_prep_pipeline.fit(df)
@@ -66,22 +50,17 @@ def index():
         #load model and make prediction
         model = NaiveBayesModel.load('Trade_Predictor_Model')
         prediction = model.transform(cleaned).select('prediction').toPandas()
-        
         prediction = prediction['prediction'].values[0]
 
         #transform 0, 1, 2 to hold/sell/buy
-
         if prediction == 0:
-            prediction = 'hold'
+            prediction = 'Hold Stocks'
         elif prediction == 1:
-            prediction = 'sell'
+            prediction = 'Sell Stocks'
         else:
-            prediction = 'buy'        
-
+            prediction = 'Buy more Stocks'        
 
     return render_template('index.html', action = prediction)#, teams=teams)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
